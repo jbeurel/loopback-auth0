5de1b7d86518c86054c70077a591f2d89937e0a6
Jonathan Beurel <jonathanb@theodo.fr>
2016-11-05 18:44:18 +0100

    JWT verification works

--- common/models/user.js
+++ common/models/user.js
@@ -4,10 +4,16 @@ var jwt = require('jsonwebtoken');
 
 module.exports = function(User) {
   User.auth0 = function (idToken, done) {
-    console.log('coucou idToken', idToken);
-    var decoded = jwt.verify(idToken, 'ckhc7l-mcoUXp50Ze2TG7zERNc8axo70or3hS7ihVcr_zf6kF7Ewb7L0MU5braFR');
-    console.log('coucou decoded', decoded);
-    done();
+    var secret = 'LetJoLiam';
+
+    jwt.verify(
+      idToken,
+      secret,
+      function(err, decoded) {
+        console.log('coucou err', err);
+        console.log('coucou decoded', decoded);
+        done();
+    });
   };
 
   User.remoteMethod('auth0', {
