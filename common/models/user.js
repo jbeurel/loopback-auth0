5dc28de1baa32631b013b66c9bd3eb6cd9453acc
Jonathan Beurel <jonathanb@theodo.fr>
2016-11-06 13:35:01 +0100

    Hide unused remote methods

--- common/models/user.js
+++ common/models/user.js
@@ -13,6 +13,37 @@ var auth0 = new AuthenticationClient({
 });
 
 module.exports = function(User) {
+
+  User.disableRemoteMethod("login", true);
+  User.disableRemoteMethod("upsert", true);
+  User.disableRemoteMethod("create", true);
+  User.disableRemoteMethod("createOrUpdate", true);
+  User.disableRemoteMethod("replaceOrCreate", true);
+  User.disableRemoteMethod("deleteById", true);
+  User.disableRemoteMethod("updateAll", true);
+  User.disableRemoteMethod("updateAttributes", false);
+  User.disableRemoteMethod("createChangeStream", true);
+  User.disableRemoteMethod("find", true);
+  User.disableRemoteMethod("findById", true);
+  User.disableRemoteMethod("findOne", true);
+  User.disableRemoteMethod("confirm", true);
+  User.disableRemoteMethod("count", true);
+  User.disableRemoteMethod("exists", true);
+  User.disableRemoteMethod("invoke", true);
+  User.disableRemoteMethod("upsertWithWhere", true);
+  User.disableRemoteMethod("replaceById", true);
+  User.disableRemoteMethod("resetPassword", true);
+
+  User.disableRemoteMethod("__count__accessTokens", false);
+  User.disableRemoteMethod("__create__accessTokens", false);
+  User.disableRemoteMethod("__delete__accessTokens", false);
+  User.disableRemoteMethod("__destroyById__accessTokens", false);
+  User.disableRemoteMethod("__findById__accessTokens", false);
+  User.disableRemoteMethod("__get__accessTokens", false);
+  User.disableRemoteMethod("__updateById__accessTokens", false);
+
+  User.validatesUniquenessOf('user_id');
+
   User.auth0 = function (authResult, done) {
     var secret = 'LetJoLiam';
 
